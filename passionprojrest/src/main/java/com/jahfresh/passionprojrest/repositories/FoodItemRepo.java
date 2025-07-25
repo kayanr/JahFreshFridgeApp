4df30b2d739c62b218c934a6264baba8dc05df7e
Kayan <kayan.reid@gmail.com>
2025-07-25 13:35:10 -0400

    Updated the Repository file to include findAll and findFoodItemById methods.

--- passionprojrest/src/main/java/com/jahfresh/passionprojrest/repositories/FoodItemRepo.java
+++ passionprojrest/src/main/java/com/jahfresh/passionprojrest/repositories/FoodItemRepo.java
@@ -2,6 +2,12 @@ package com.jahfresh.passionprojrest.repositories;
 
 import com.jahfresh.passionprojrest.models.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface FoodItemRepo extends JpaRepository<FoodItem, Long> {
+    FoodItem findFoodItemById(Long id);
+    List<FoodItem> findAll();
 }
