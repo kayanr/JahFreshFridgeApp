4b7c29582c0c9c47076a95af98b45cef41a0bf91
Kayan <kayan.reid@gmail.com>
2026-03-17 11:30:41 -0400

    Update  BASE_URL to point to the new backend path

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = '/api/fooditems';
+const API_HOST = 'http://localhost:8080';
+const BASE_URL = `${API_HOST}/api/fooditems`;
 
 async function getAllFoodItems(page = 0, size = 5) {
     const response = await fetch(`${BASE_URL}?page=${page}&size=${size}`);
