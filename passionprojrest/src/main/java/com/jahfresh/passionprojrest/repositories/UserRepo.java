eb3f760613de3cf0d08e2f91a59e33e98dbf3015
Kayan <kayan.reid@gmail.com>
2026-03-18 10:19:46 -0400

    Add Spring Security and JJWT dependencies. Update User entity to implement UserDetails with username, password, and role fields. Add findByUsername, existsByUsername, and existsByEmail to UserRepo.

--- passionprojrest/src/main/java/com/jahfresh/passionprojrest/repositories/UserRepo.java
+++ passionprojrest/src/main/java/com/jahfresh/passionprojrest/repositories/UserRepo.java
@@ -3,5 +3,10 @@ package com.jahfresh.passionprojrest.repositories;
 import com.jahfresh.passionprojrest.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
